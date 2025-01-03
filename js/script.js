098bd40d2ae2e51ba576c3b1bff42c2264a45a4e
marco-gulino <marcogulino98@gmail.com>
2025-01-03 21:44:37 +0100

    [feat/js] random number generate when pressing the "new-number" button and add a refresh page function when pressing the "new-game" button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
+// FILL CLICKED CIRCLES
 const groupCircles = document.querySelectorAll(".circle");
 
 groupCircles.forEach((groupCircle) => {
-    groupCircle.addEventListener('click', () => {
-      groupCircle.style.backgroundColor = "rgb(247, 140, 164)";
-      groupCircle.style.boxShadow = "-5px 4px 0 rgb(205, 32, 33)";
-    });
+  groupCircle.addEventListener("click", () => {
+    groupCircle.style.backgroundColor = "rgb(247, 140, 164)";
+    groupCircle.style.boxShadow = "-5px 4px 0 rgb(205, 32, 33)";
+  });
+});
+
+// RANDOM NUMBER GENERATOR
+const num = document.getElementById("square");
+const btn = document.getElementById("new-number");
+let arrayNum = [];
+let z = 0;
+
+//fill array of numbers from 1 to 90
+for (let j = 1; j < 91; j++) {
+  arrayNum.push(j);
+}
+
+btn.addEventListener("click", () => {
+  if (arrayNum.length < 1) {
+    document.location.reload();
+  }
+  console.log("array length: " + arrayNum.length);
+  z = Math.floor(Math.random() * arrayNum.length);
+  console.log("index sorted: " + z);
+  num.innerHTML = arrayNum[z];
+  arrayNum.splice(z, 1);
+});
+
+// RESET PAGE
+const resetBtn = document.getElementById("new-game");
+
+resetBtn.addEventListener("click", () => {
+  document.location.reload();
 });
